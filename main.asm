83f59380fa0113ae51d093bd335e149d3bafedf0
techNFCelevator <152115329+techNFCelevator@users.noreply.github.com>
2025-03-13 18:53:40 -0700

    BSS section created

--- main.asm
+++ main.asm
@@ -1,3 +1,10 @@
 SECTION .data
 msg1    db  'Please input a value up to the nearest hundredth: ', 0h
 msg2    db  'After GST factored: ', 0h
+
+SECTION .bss
+input:  RESQ    1   ;
+tax:    RESQ    1   ;
+GST:    RESQ    1   ;
+output: RESQ    1   ;
+
