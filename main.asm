bc16a8c081734466da7f36e1ae657681b3b937e5
techNFCelevator <152115329+techNFCelevator@users.noreply.github.com>
2025-03-14 12:35:11 -0700

    Changed float variables from quadword to doubleword.

--- main.asm
+++ main.asm
@@ -1,14 +1,14 @@
 %include    'functions.asm'
 
 SECTION .data
-gst     dq  0.05
+gst     dd  0.05
 msg1    db  'Please input a value up to the nearest hundredth: ', 0h
 msg2    db  'After GST factored: ', 0h 
 
 SECTION .bss
-input:  RESQ    8
-tax:    RESQ    8
-output: RESQ    8
+input:  RESD    8
+tax:    RESD    8
+output: RESD    8
 
 SECTION .text
 global _start
