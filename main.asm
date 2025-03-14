008f52fd90e5890e03a57d0e2433a67b71612ae0
techNFCelevator <152115329+techNFCelevator@users.noreply.github.com>
2025-03-14 11:57:03 -0700

    Added output printing to terminal.

--- main.asm
+++ main.asm
@@ -1,14 +1,14 @@
 %include    'functions.asm'
 
 SECTION .data
-gst     dd  0.05
+gst     dq  0.05
 msg1    db  'Please input a value up to the nearest hundredth: ', 0h
 msg2    db  'After GST factored: ', 0h 
 
 SECTION .bss
 input:  RESQ    8
-output: RESQ    8
 tax:    RESQ    8
+output: RESQ    8
 
 SECTION .text
 global _start
@@ -25,6 +25,9 @@ _start:
     int     80h
     
     mov     eax, msg2
+    call    strprint
+
+    mov     eax, output
     call    strprint
 
     call    quit
