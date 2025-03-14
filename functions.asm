8dfb097aed003ac53cf4aea35df3cf61ff89786a
techNFCelevator <152115329+techNFCelevator@users.noreply.github.com>
2025-03-13 20:48:38 -0700

    Eliminated ';' in BSS section, added separate functions.asm file, provisioned external file inclusion in the main.asm file, and expanded _start subroutine.

---
+++ functions.asm
@@ -0,0 +1,34 @@
+strlen:
+    push    ebx
+    mov     ebx, eax
+
+nextchar:
+    cmp     byte [eax], 0
+    jz      finished
+    inc     eax
+    jmp     nextchar
+
+finished:
+    sub     eax, ebx
+    pop     ebx
+    ret
+
+strprint:
+    push    edx
+    push    ecx
+    push    ebx
+    push    eax
+    call    strlen
+
+    mov     edx, eax
+    pop     eax
+    
+    mov     ecx, eax
+    mov     ebx, 1
+    mov     eax, 4
+    int     80h
+
+    pop     ebx
+    pop     ecx
+    pop     edx
+    ret
--- main.asm
+++ main.asm
@@ -1,10 +1,28 @@
+%include    'functions.asm'
+
 SECTION .data
 msg1    db  'Please input a value up to the nearest hundredth: ', 0h
 msg2    db  'After GST factored: ', 0h
 
 SECTION .bss
-input:  RESQ    1   ;
-tax:    RESQ    1   ;
-gst:    RESQ    1   ;
-output: RESQ    1   ;
+input:  RESQ    1
+tax:    RESQ    1
+gst:    RESQ    1
+output: RESQ    1
 
+SECTION .text
+global _start
+
+_start:
+
+    mov     eax, msg1
+    call    strprint
+
+    mov     edx, 1
+    mov     ecx, input
+    mov     ebx, 0
+    mov     eax, 3
+    int     80h
+    
+    mov     eax, msg2
+    call    strprint
